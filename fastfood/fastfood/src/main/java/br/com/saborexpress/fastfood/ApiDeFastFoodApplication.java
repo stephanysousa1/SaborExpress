d447689c681b361a636b60ee33ebbdd186088dbc
stephanysousa1 <stephanysousah123@gmail.com>
2025-05-24 09:23:07 -0300

    Gerando a pasta do SpringBoot

---
+++ fastfood/fastfood/src/main/java/br/com/saborexpress/fastfood/ApiDeFastFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.saborexpress.fastfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeFastFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeFastFoodApplication.class, args);
+	}
+
+}
---
+++ fastfood/fastfood/src/test/java/br/com/saborexpress/fastfood/ApiDeFastFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.saborexpress.fastfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeFastFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
